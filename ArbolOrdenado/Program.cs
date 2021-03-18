60e6e534e2ba0279526b9a4d084e9d29351bc4c6
Cesarin360 <josue360cjpimentel@gmail.com>
2021-03-18 11:52:37 -0600

    V1

---
+++ ArbolOrdenado/Program.cs
@@ -0,0 +1,33 @@
+﻿using ArbolOrdenado.Clases.ArbolBinarioOrdenado;
+using System;
+
+namespace ArbolOrdenado
+{
+    class Program
+    {
+        public static void pruebaOrden()
+        {
+            ArbolBinarioBusqueda ArBus = new ArbolBinarioBusqueda();
+            String[] datos = { "Cesar", "Vanessa", "Anthony", "Jeff", "Sarai", "Pedro", "Antoni", "Michel" };
+            Estudiante es = new Estudiante();
+            foreach (var d in datos)
+            {
+                es.nombre = d;
+                es.numMat = $"insertar No.{d}";
+                ArBus.insertar(es);
+                es = new Estudiante();
+            }
+            int pausa;
+            pausa = 0;
+            es = new Estudiante();
+            es.nombre = "Jeff";
+            es.numMat = "cualquier cosa";
+            ArBus.eliminar(es);
+            pausa = 0;
+        }
+        static void Main(string[] args)
+        {
+            pruebaOrden();
+        }
+    }
+}
